f506301f6f4423ffc74d0513bcb38e6adfb25283
Oliver Schantz <frequency403@gmail.com>
2024-01-09 17:25:41 +0100

    Progress for ServerCommunicator.cs

--- OpenSSHALib/Lib/ServerCommunicator.cs
+++ OpenSSHALib/Lib/ServerCommunicator.cs
@@ -2,13 +2,14 @@
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
+using OpenSSHALib.Models;
 using Renci.SshNet;
 
 namespace OpenSSHALib.Lib;
 
 public static class ServerCommunicator
 {
-    public static bool TestConnection(string ipAdressOrHostname, [NotNullWhen(false)]out string? message)
+    public static bool TestConnection(string ipAddressOrHostname, [NotNullWhen(false)]out string? message)
     {
         message = null;
         if (!NetworkInterface.GetIsNetworkAvailable())
@@ -18,24 +19,56 @@ public static class ServerCommunicator
         }
 
         using var ping = new Ping();
-        return ping.Send(ipAdressOrHostname).Status == IPStatus.Success;
+        return ping.Send(ipAddressOrHostname).Status == IPStatus.Success;
     }
 
-    public static bool PingSshConnection(string ipAdressOrHostname, string user, string userPassword,[NotNullWhen(false)] out string? message)
+    public static bool TryOpenSshConnection(string ipAddressOrHostname, string user, string userPassword,[NotNullWhen(true)] out SshClient? connection, [NotNullWhen(false)] out string? message)
     {
+        connection = null;
         message = null;
         try
         {
-            using var sshClient = new SshClient(ipAdressOrHostname, user, userPassword);
+            var sshClient = new SshClient(ipAddressOrHostname, user, userPassword);
             sshClient.Connect();
             var connectionResult = sshClient.IsConnected;
-            sshClient.Disconnect();
+            if (!connectionResult) return connectionResult;
+            var checkOs = sshClient.RunCommand("uname -s");
+            if (!checkOs.Result.Contains("linux", StringComparison.CurrentCultureIgnoreCase)) throw new NotSupportedException("KeyToServer upload does not support any other OS than Linux!");
+            connection = sshClient;
             return connectionResult;
         }
         catch (Exception e)
         {
             message = e.Message;
             return false;
+        }
+    }
+
+    private static bool CreateAuthorizedKeysIfNotExist(this SshClient clientConnection, [NotNullWhen(false)] out string? message)
+    {
+        message = null;
+        var listDirectory = clientConnection.RunCommand("ls $HOME/.ssh");
+        if (listDirectory.Result.Split("\n", StringSplitOptions.RemoveEmptyEntries).Contains("authorized_keys")) return true;
+        var createAuthorizedKeysFile = clientConnection.RunCommand("touch $HOME/.ssh/authorized_keys && chmod 700 $HOME/.ssh/authorized_keys");
+        if (createAuthorizedKeysFile.ExitStatus == 0) return true;
+        message = createAuthorizedKeysFile.Error + "\n" + createAuthorizedKeysFile.Result;
+        return false;
+    }
+    
+    public static async Task<string> PutKeyToServer(this SshClient clientConnection, SshPublicKey publicKey)
+    {
+        try
+        {
+            if (!clientConnection.CreateAuthorizedKeysIfNotExist(out var errorMessage)) throw new ApplicationException(errorMessage);
+            var export = await publicKey.ExportKey();
+            if (export is null) return "Key could not be exported!";
+            var command = clientConnection.RunCommand($"echo \"{export}\r\n\" >> $HOME/.ssh/authorized_keys");
+            if (command.ExitStatus != 0) throw new Exception(command.Error + "\n" + command.Result);
+            return "Key successfully uploaded";
+        }
+        catch (Exception e)
+        {
+            return e.Message;
         }
     }
 }
