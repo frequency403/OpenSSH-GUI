4594ed2c598513734b57698757f3d82a6155c625
Dom <dom@futurecolossal.com>
2024-04-22 16:59:50 -0400

    error handling on initial key load

--- OpenSSHALib/Lib/DirectoryCrawler.cs
+++ OpenSSHALib/Lib/DirectoryCrawler.cs
@@ -1,14 +1,30 @@
 ﻿using OpenSSHALib.Extensions;
 using OpenSSHALib.Models;
+using Renci.SshNet.Security;
 
 namespace OpenSSHALib.Lib;
 
 public static class DirectoryCrawler
 {
-    public static IEnumerable<SshPublicKey> GetAllKeys()
+    public readonly record struct SshCrawlError(string File, Exception Exception);
+    
+    public static IEnumerable<SshPublicKey> GetAllKeys(out List<SshCrawlError> errors)
     {
+        var errorList = new List<SshCrawlError>();
+        errors = errorList;
         return Directory.EnumerateFiles(SshConfigFilesExtension.GetBaseSshPath(), "*.pub", SearchOption.AllDirectories)
-            .Select(
-                e => new SshPublicKey(e));
+            .Select(filePath =>
+            {
+                try
+                {
+                    return new SshPublicKey(filePath);
+                }
+                catch (Exception ex)
+                {
+                    errorList.Add(new SshCrawlError(filePath, ex));
+                    return null;
+                }
+            })
+            .Where(x => x != null)!;
     }
 }
