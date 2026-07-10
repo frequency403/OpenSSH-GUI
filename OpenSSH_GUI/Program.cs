6d8a150fbebc3277133787af7e0ab30acc034483
Oliver Schantz <frequency403@gmail.com>
2026-07-10 09:15:55 +0200

    Made assembly a parameter for future testing

--- OpenSSH_GUI/Program.cs
+++ OpenSSH_GUI/Program.cs
@@ -28,11 +28,10 @@ internal sealed class Program
     public const string AppName = "OpenSSH GUI";
     public const string VersionEnvVar = "RUNNING_VERSION";
 
-    private static string GetHostVersion() => Assembly.GetEntryAssembly()
-                                                  ?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
-                                                  ?.InformationalVersion
-                                              ?? Assembly.GetEntryAssembly()?.GetName().Version?.ToString()
-                                              ?? "0.0.0";
+    private static string GetHostVersion(Assembly? assembly) => assembly?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                                                                    ?.InformationalVersion
+                                                                ?? Assembly.GetEntryAssembly()?.GetName().Version?.ToString()
+                                                                ?? "0.0.0";
 
     private static void ConfigureOpenSshGuiLogger(
         ApplicationConfiguration bootstrapConfig,
@@ -108,7 +107,7 @@ internal sealed class Program
 
         configurationBuilder.AddInMemoryCollection(
         [
-            new KeyValuePair<string, string?>(VersionEnvVar, GetHostVersion())
+            new KeyValuePair<string, string?>(VersionEnvVar, GetHostVersion(Assembly.GetEntryAssembly()))
         ]);
     }
 
